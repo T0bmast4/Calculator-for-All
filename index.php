e0ccc140589937f30fcf3d00b6b90092a13bed6b
cnkillerio <fahim.9999hamidi@gmail.com>
2023-11-16 11:53:50 +0100

    neue css

--- index.php
+++ index.php
@@ -3,18 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        button {
+            background-color: #007BFF; 
+            color: white;
+            padding: 10px;
+            font-size: 16px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            margin-bottom: 10px; 
+            width: 150px; 
+        }
+
+        button:hover {
+            background-color: #0056b3; 
+        }
+    </style>
     <title>Calculator for All</title>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <h1>Willkommen bei Calculator for All</h1>
     <h3>Hier kannst du alle Rechnungen automatisch ausrechnen, welche in der 3AFI derzeit unterrichtet werden.</h3>
-    <p>Im laufe der Zeit werden noch weitere Updates veröffentlicht!</p>
+    <p>Im Laufe der Zeit werden noch weitere Updates veröffentlicht!</p>
+    <br><br>
+    <form action="mathe_overview.php" method="get">
+        <button type="submit">Mathematik</button>
+    </form>
     <br>
-    <ul>
-        <li><a href="mathe_overview.php">Mathematik</a></li>
-        <li><a href="elektrotechnik_overview.php">Elektrotechnik</a></li>
-        <li><a href="betp_overview.php">Betriebstechnik</a></li>
-    </ul>
+    <form action="elektrotechnik_overview.php" method="get">
+        <button type="submit">Elektrotechnik</button>
+    </form>
+    <br>
+    <form action="betp_overview.php" method="get">
+        <button type="submit">Betriebstechnik</button>
+    </form>
 </body>
 </html>
