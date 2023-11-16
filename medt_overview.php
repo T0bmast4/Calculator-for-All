71b96b6831849266c1d314f066cf529b1e2c569b
T0bmast4 <99271449+T0bmast4@users.noreply.github.com>
2023-11-16 10:56:43 +0100

    Medientechnik Form_Creator

---
+++ medt_overview.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medientechnik-Übersicht</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Medientechnik-Übersicht</h1>
+    <ul>
+        <li><a href="medientechnik/form_creator.php">Formular-Creator</a></li>
+    </ul>
+</body>
+</html>
