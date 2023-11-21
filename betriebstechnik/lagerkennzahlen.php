21d08a104c6eee673a91673d59b5558e75511ee7
Kratus1501 <markushirnich1501@gmail.com>
2023-11-21 10:39:33 +0100

    lager

--- betriebstechnik/lagerkennzahlen.php
+++ betriebstechnik/lagerkennzahlen.php
@@ -23,15 +23,48 @@
 
         <label for="quartal4">4. Quartal</label>
         <input type="text" name="quartal4" id="quartal4" value="0" required>
+
+        <label for="warenabgänge">Warenabgänge pro Monat</label>
+        <input type="text" name="warenabgänge" id="warenabgänge" value="0" required>
+
+        <label for="preis">Wert pro Stück</label>
+        <input type="text" name="preis" id="preis" value="0" required>
+
+        <label for="lagerhaltungskostensatz">Lagerhaltungskostensatz</label>
+        <input type="text" name="lagerhaltungskostensatz" id="lagerhaltungskostensatz" value="0" required>
+
+        <input type="submit" value="Berechnen">
     </form>
 
     <?php
-        if (isset($_POST['anfangsbestand'], $_POST['quartal1'], $_POST['quartal2'], $_POST['quartal3'], $_POST['quartal4'])){
+        if (isset($_POST['anfangsbestand'], $_POST['quartal1'], $_POST['quartal2'], $_POST['quartal3'], $_POST['quartal4'], $_POST['warenabgänge'], $_POST['preis'], $_POST['lagerhaltungskostensatz'])){
             $an = $_POST['anfangsbestand'];
             $q1 = $_POST['quartal1'];
             $q2 = $_POST['quartal2'];
             $q3 = $_POST['quartal3'];
             $q4 = $_POST['quartal4'];
+            $wa = $_POST['warenabgänge'];
+            $pr = $_POST['preis'];
+            $lks = $_POST['lagerhaltungskostensatz'];
+
+            $durchschnittlagerbestand = ($an + $q1 + $q2 + $q3 + $q4) / 5;
+
+            $jahrverbrauch = 12 * $wa;
+
+            $lagerumschlagshäufigkeit = $wa / $durchschnittlagerbestand;
+
+            $durchschnittlagerdauer = 360 / $lagerumschlagshäufigkeit;
+
+            $kapitalbindung = $durchschnittlagerbestand * $pr;
+
+            $lagerhaltungskosten = $kapitalbindung * (0.01 * $lks);
+
+            echo"Durchschnittlicher Lagerbestand = " .$durchschnittlagerbestand. "<br>";
+            echo"Jährlicher Verbrauch = " .$jahrverbrauch. "<br>";
+            echo"Lagerumschlagshäufigkeit = " .$lagerumschlagshäufigkeit. "<br>";
+            echo"Durchschnittliche Lagerdauer = " .$durchschnittlagerdauer. "<br>";
+            echo"Durchschnittliche Kapitalbindung = " .$kapitalbindung. "<br>";
+            echo"Lagerhaltungskosten = " .$lagerhaltungskosten. "<br>";
 
         }
     ?>
